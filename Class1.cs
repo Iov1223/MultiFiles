9e4889d38a9d557b1c80263a386f67e0a92f935b
Academy_ekat@itstep.academy <Academy_ekat@itstep.academy>
2022-08-15 21:11:42 +0500

    Add project files.

---
+++ Class1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MultiFiles
+{
+    class Class1
+    {
+        static public void Plus(string num1, string num2)
+        {
+            double n1 = Convert.ToInt32(num1);
+            double n2 = Convert.ToInt32(num2);
+            double res = n1 + n2;
+            Console.WriteLine("{0} + {1} = {2}", n1, n2, res);
+        }
+        static public void Minus(string num1, string num2)
+        {
+            double n1 = Convert.ToInt32(num1);
+            double n2 = Convert.ToInt32(num2);
+            double res = n1 - n2;
+            Console.WriteLine("{0} - {1} = {2}", n1, n2, res);
+        }
+        static public void Multiply(string num1, string num2)
+        {
+            double n1 = Convert.ToInt32(num1);
+            double n2 = Convert.ToInt32(num2);
+            double res = n1 * n2;
+            Console.WriteLine("{0} * {1} = {2}", n1, n2, res);
+        }
+        static public void Divide(string num1, string num2)
+        {
+            double n1 = Convert.ToInt32(num1);
+            double n2 = Convert.ToInt32(num2);
+            if (n2 == 0)
+            {
+                Console.WriteLine("На нодь делить нельзя");
+            }
+            else
+            {
+                double res = n1 / n2;
+                Console.WriteLine("{0} / {1} = {2}", n1, n2, res);
+            }
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace MultiFiles
+{
+    delegate void myDelegate(string myStr, string myStr1);
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите 1-ое число:");
+            string str1 = Console.ReadLine();
+            Console.WriteLine("Введите 1-ое число:");
+            string str2 = Console.ReadLine();
+            Console.WriteLine("Какое действие произвести (+ - сложить, - - вычесть," +
+                "* - умножить, / - разделить):");
+            string choice = Console.ReadLine();
+            myDelegate calc = Class1.Plus;
+            if (choice == "+")
+            {
+                calc(str1, str2);
+            }
+            else
+            {
+                if (choice == "-")
+                {
+                    calc = Class1.Minus;
+                    calc(str1, str2);
+                }
+                else
+                {
+                    if (choice == "*")
+                    {
+                        calc = Class1.Multiply;
+                        calc(str1, str2);
+                    }
+                    else
+                    {
+                        if (choice == "/")
+                        {
+                            calc = Class1.Divide;
+                            calc(str1, str2);
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
